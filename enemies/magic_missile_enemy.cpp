213b143be834788ec39182af73026738c7853bb5
Seina <32441141+SaynaHana@users.noreply.github.com>
2025-04-02 11:26:03 -0400

    Changed magic missile enemy movement

--- enemies/magic_missile_enemy.cpp
+++ enemies/magic_missile_enemy.cpp
@@ -1,9 +1,11 @@
 #include "magic_missile_enemy.h"
+#include <glm/gtx/string_cast.hpp>
 
 namespace game {
     MagicMissileEnemy::MagicMissileEnemy(const glm::vec3 &position, game::Geometry *geom, game::Shader *shader,
                                      GLuint texture, int health, game::MoveData &move_data, game::Weapon *weapon)
                                      : EnemyGameObject(position, geom, shader, texture, health, move_data, weapon) {
+		t_ = 0;
 		go_right_ = false;
     }
 
@@ -13,24 +15,29 @@ namespace game {
 
     }
 	void MagicMissileEnemy::Move(double delta_time) {
-		EnemyGameObject::Move(delta_time);
-
 		glm::vec3 side_movement = glm::vec3(0);
-
 
 		if (go_right_) {
 			t_ += delta_time;
-			side_movement = glm::vec3(t_, sin(t_), 0);
+			side_movement = glm::vec3(0, 3 * sin(t_), 0);
 
 			if (t_ > 1) go_right_ = false;
 		}
 		else {
 			t_ -= delta_time;
-			side_movement = glm::vec3(-t_, sin(t_), 0);
+			side_movement = glm::vec3(0, 3 * sin(t_), 0);
 
-			if (t_ < -1) go_right_ = true;
+			if (t_ < -0.5f) go_right_ = true;
 		}
 
-		SetPosition(GetPosition() + side_movement);
+		if (target_ == nullptr) return;
+
+		glm::vec3 diff = target_->GetPosition() - GetPosition();
+
+		if (glm::length(diff) != 0) {
+			diff = glm::normalize(diff);
+		}
+
+		SetPosition(GetPosition() + diff * (float)delta_time + side_movement * (float)delta_time);
 	}
 };
